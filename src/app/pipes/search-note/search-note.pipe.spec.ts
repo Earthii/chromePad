8009d3542f4b4f726fa09061f67430066cf267c6
Eric Xiao <xiao_eric@hotmail.com>
2018-05-31 17:43:52 -0400

    removed tests, no time for test for this scope of project

--- src/app/pipes/search-note/search-note.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SearchNotePipe } from './search-note.pipe';
-
-describe('SearchNotePipe', () => {
-  it('create an instance', () => {
-    const pipe = new SearchNotePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
